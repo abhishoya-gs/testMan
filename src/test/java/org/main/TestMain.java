01357b0616c428f2949002a0784baa803cb16976
Abhishoya Lunavat <Abhishoya.Lunavat@gs.com>
2021-08-06 17:22:28 +0530

    patch

--- src/test/java/org/main/TestMain.java
+++ src/test/java/org/main/TestMain.java
@@ -1,10 +1,17 @@
 package org.main;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.util.Properties;
 
 public class TestMain {
     @Test
-    public void testSample() {
-        System.out.println("\n\n\nPrinting System Variable: " + System.getenv("aws_key") + "\n\n\n");
+    public void testSample() throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/test/resources/test.properties"));
+        String s = System.getenv(properties.getProperty("s"));
+        System.out.println("\n\n\nPrinting System Variable: " + s + "\n\n\n");
     }
 }
