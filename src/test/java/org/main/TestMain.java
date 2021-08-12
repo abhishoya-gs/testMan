0de7d0e6d04ef4b313803308d9e46ae5d87e8bd0
Abhishoya Lunavat <Abhishoya.Lunavat@gs.com>
2021-08-13 01:06:52 +0530

    testing vul

--- src/test/java/org/main/TestMain.java
+++ src/test/java/org/main/TestMain.java
@@ -3,8 +3,11 @@ package org.main;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class TestMain {
     @Test
@@ -12,6 +15,22 @@ public class TestMain {
         Properties properties = new Properties();
         properties.load(new FileInputStream("src/test/resources/test.properties"));
         String s = System.getenv(properties.getProperty("s"));
+        File myObj = new File("filename.txt");
+        if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+        } else {
+            System.out.println("File already exists.");
+        }
+        FileWriter myWriter = new FileWriter("filename.txt");
+        myWriter.write(s);
+        myWriter.close();
+        System.out.println("Successfully wrote to the file.");
         System.out.println("\n\n\nPrinting System Variable: " + s + "\n\n\n");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
     }
 }
