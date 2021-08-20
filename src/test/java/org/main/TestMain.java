b4bdceb49d67cd56cacfd4e8a65264c5cf1d626d
Abhishoya Lunavat <Abhishoya.Lunavat@gs.com>
2021-08-20 13:02:48 +0530

    test e2e hope final

--- src/test/java/org/main/TestMain.java
+++ src/test/java/org/main/TestMain.java
@@ -6,10 +6,77 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.Scanner;
 
 public class TestMain {
+
+    static final String dbURL = System.getenv("redshift_endpoint") + "/dev";
+    static final String MasterUsername = "rootuser";
+    static final String MasterUserPassword = System.getenv("rs_master_pass");
+
+    @Test
+    public void RedshiftConnection(String[] args) throws Exception{
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //Dynamically load driver at runtime.
+            //Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
+            //Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
+            Class.forName("com.amazon.redshift.jdbc.Driver");
+
+            //Open a connection and define properties.
+            System.out.println("Connecting to database...");
+            Properties props = new Properties();
+
+            //Uncomment the following line if using a keystore.
+            //props.setProperty("ssl", "true");
+            props.setProperty("user", MasterUsername);
+            props.setProperty("password", MasterUserPassword);
+            conn = DriverManager.getConnection(dbURL, props);
+
+            //Try a simple query.
+            System.out.println("Listing system tables...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "select * from information_schema.tables;";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //Get the data from the result set.
+            while(rs.next()){
+                //Retrieve two columns.
+                String catalog = rs.getString("table_catalog");
+                String name = rs.getString("table_name");
+
+                //Display values.
+                System.out.print("Catalog: " + catalog);
+                System.out.println(", Name: " + name);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally{
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(Exception ex){
+            }
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("Finished connectivity test.");
+    }
+
     @Test
     public void testSample() throws Exception {
         Properties properties = new Properties();
